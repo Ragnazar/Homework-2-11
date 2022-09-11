018a180fd9e960bb95e5972a7dfed43950d59e8e
ragnazar <ivan.avdeyev@wilix.org>
2022-09-11 14:50:53 +0300

    initial-commit

---
+++ src/test/java/pro/sky/homeworks/homework211/Homework211ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworks.homework211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
