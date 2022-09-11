b441e830481ce14c983832e6f2b34b0d60be23ac
ragnazar <ivan.avdeyev@wilix.org>
2022-09-11 17:25:36 +0300

    add-session-scope-to-bucket-service

--- src/main/java/pro/sky/homeworks/homework211/services/BasketServiceImpl.java
+++ src/main/java/pro/sky/homeworks/homework211/services/BasketServiceImpl.java
@@ -1,12 +1,14 @@
 package pro.sky.homeworks.homework211.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 import pro.sky.homeworks.homework211.dto.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@SessionScope
 public class BasketServiceImpl implements BasketService {
     private final List<Product> basket = new ArrayList<>();
 
